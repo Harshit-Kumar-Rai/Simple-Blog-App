e0a306875c041a80f5100fd4a1288455373aa38c
Harshit Kumar Rai <106249416+Harshit-Kumar-Rai@users.noreply.github.com>
2023-06-30 15:08:25 +0530

    Add files via upload

---
+++ blog/app/urls.py
@@ -0,0 +1,10 @@
+from .import views
+from django.urls import path
+
+urlpatterns = [
+
+    path("", views.index),
+    path("addPost/", views.addPost, name='addPost'),
+    path('readPost/<int:id>', views.detailedBlog, name='readPost'),
+    
+]
---
+++ blog/app/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from .models import BlogPost
+# Create your views here.
+def index(request):
+
+    post = BlogPost.objects.all()
+
+    return render(request, 'index.html',{'post':post})
+
+def addPost(request):
+    try:
+        author = request.POST.get('author')
+        title  = request.POST.get('title')
+        short = request.POST.get('short')
+        post = request.POST.get('post')
+
+        print(author, title, short, post)
+
+        b = BlogPost.objects.create(model_author = author, model_title = title, model_short = short, model_post = post)
+        b.save()
+
+    except Exception as e:
+        print(e)
+
+    return render(request, 'addPost.html')
+
+def detailedBlog(request, id):
+
+    data = BlogPost.objects.get(model_id = id)
+
+    return render(request, 'readPost.html', {'id':data})
